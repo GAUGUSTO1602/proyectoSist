ab602dbaf0be71eb57a75d0642a5e507b7a94f6c
GAUGUSTO1602 <you@example.com>
2023-02-19 10:27:02 -0400

    adelantoLandingPage

--- proyectoinicio/src/App.jsx
+++ proyectoinicio/src/App.jsx
@@ -5,9 +5,52 @@ import './App.css'
 function App() {
 
   return (
-    <div className="App">
+    <div className='Container'>
       
+      <header class="header">
+          <nav>
+              
+              <div class="logo">
+                  <img src="img/Logo.png" alt="" />
+              </div>
+
+              <ul class="navLinks">
+                  <li>Home</li>
+                  <li>Sobre nosotros</li>
+                  <li>Doctores</li>
+                  <li>Iniciar Sesión</li>
+                  <li>Registrarse</li>
+              </ul>
+
+          </nav>
+
+      </header>
+
+      <section class="block1">
+
+        <div class="imageBlock1">
+            <img src="Img\Foto inicio.png" alt=""/>
+        </div>
+
+        <div class="textBlock1">
+            
+            <div class="titleBlock1">
+                <h1>
+                    Psicopana
+                </h1>
+            </div>
+
+            <div class="parragraphBlock1">
+                <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type spublishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+            </div>
+
+        </div>
+    </section>
+
+
     </div>
+
+    
   )
 }
 
