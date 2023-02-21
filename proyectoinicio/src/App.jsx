39492568b3c8a4ac183e15d29b5c02db2cbda8aa
GAUGUSTO1602 <you@example.com>
2023-02-21 14:10:35 -0400

    adelanto

--- proyectoinicio/src/App.jsx
+++ proyectoinicio/src/App.jsx
@@ -12,27 +12,87 @@ function App() {
             <img class="imagen" src="img/Logo.png" alt="" />
         </div>
 
-        <img class="imagen2" src="img/img2.png" alt="" />
-
-        <h3 className='titulo2'>Iniciar sesión</h3>
-
         <div className='rectangulo'></div>
 
-        <h5 className='subtitulo1' id='sub1'>Usuario</h5>
+        <h3 className='titulo2'>Regístrate como paciente.</h3>
 
-        <input type="text" class="search-field1"></input>
+        <div className='subtitulos'>
 
-        <h5 className='subtitulo2'>Contraseña</h5>
+          <h5 className='sub1'>Nombres</h5>
 
-        <input type="text" class="search-field2"></input>
+          <h5 className='sub2'>Apellidos</h5>
 
-        <button type="button" class="button1" id="searchButtom">Iniciar sesión</button>
+          <h5 className='sub3'>Número de teléfono</h5>
+
+          <h5 className='sub4'>Correo electrónico</h5>
+
+          <h5 className='sub5'>Contraseña</h5>
+
+          <h5 className='sub6'>Confirmar contraseña</h5>
+
+          <h5 className='sub7'>Edad</h5>
+
+          <h5 className='sub8'>¿Primera vez que asiste a consulta psicológica?</h5>
+
+          <h5 className='sub9'>Sí.</h5>
+
+          <h5 className='sub10'>No.</h5>
+
+          <h5 className='sub11'>Prefiero no contestar.</h5>
+
+        </div>
+
+
+        <input type="text" class="field1"></input>
+
+        <input type="text" class="field2"></input>
+
+        <input type="text" class="field3"></input>
+
+        <input type="text" class="field4"></input>
+
+        <input type="text" class="field5"></input>
+
+        <input type="text" class="field6"></input>
+
+        <input type="text" class="field7"></input>
+
+
+        <div className='radioButtons'>
+
+          <label>
+              <input className='b1' type="radio" value="Si" name='option' />
+            <span></span>
+          </label>
+
+          <label>
+            <input className='b2' type="radio" value="No" name='option' />
+            <span></span>
+          </label>
+
+          <label>
+            <input className='b3' type="radio" value="Prefiero no contestar." name='option' />
+            <span></span>
+          </label>
+
+        </div>
+
+
+        <button type="button" class="button1" id="searchButtom">Crear cuenta</button>
+
+
+        <h3 className='O'>O</h3>
+
+        <div className='linea1'></div>
+
+        <div className='linea2'></div>
+
 
         <button type="button" class="button2" id="searchButtom">Continuar con Google</button>
 
         <img class="logoGoogle" src="img/google.png" alt="" />
 
-        <h3 className='titulo3'>¿No tienes cuenta? Regístrate aquí.</h3>
+        <h3 className='titulo3'>¿Ya tienes cuenta? Inicia sesión aquí.</h3>
 
         <h5 className='titulo4'>Todos los derechos reservados</h5>
 
