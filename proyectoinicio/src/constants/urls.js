308c6ead5b3b7711ab88432b55b8de822534fa47
Jennicoleperez <77292561+Jennicoleperez@users.noreply.github.com>
2023-02-26 14:16:04 -0400

    Add files via upload

--- proyectoinicio/src/constants/urls.js
+++ proyectoinicio/src/constants/urls.js
@@ -3,5 +3,3 @@ export const LOGIN_URL = "/login";
 export const SelReg_URL = "/seleccion";
 export const RegPac_URL = "/RegPac";
 export const RegDoc_URL = "/RegDoc";
-export const PerDoc_URL = "/PerDoc";
-export const PerPac_URL = "/PerPac";
