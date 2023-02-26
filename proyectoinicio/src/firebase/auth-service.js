d7fd41e0d716f2c9c82583178c37ad06ecb3e22e
Jose Almonte <jose.almonte@correo.unimet.edu.ve>
2023-02-26 13:00:44 -0400

    Popup de Google registro pacientes

---
+++ proyectoinicio/src/firebase/auth-service.js
@@ -0,0 +1,17 @@
+// Todos los métodos de autenticación
+import {signInWithPopup} from 'firebase/auth'
+import { auth, googleProvider } from './config';
+
+export const signInWithGoogle = async () => {
+    try{
+        const result = await signInWithPopup(auth, googleProvider);
+        console.log(result);
+
+    }catch(error){
+        console.log(error);
+
+    }
+};
+export const registerWithEmailAndPassword = async () => {};
+export const signInWithEmailAndPassword = async () => {};
+export const logout = async () => {};
