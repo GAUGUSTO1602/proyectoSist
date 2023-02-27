f1e72113af375c2d5e28d5d729e2a6f96a264a7d
Jose Almonte <jose.almonte@correo.unimet.edu.ve>
2023-02-27 11:22:10 -0400

    El programa ya sabe que inició sesión por email

--- proyectoinicio/src/context/UserContext.jsx
+++ proyectoinicio/src/context/UserContext.jsx
@@ -1,6 +1,7 @@
 import { onAuthStateChanged } from "@firebase/auth";
 import React, {useContext, createContext, useEffect, useState} from "react";
 import { auth } from "../firebase/config";
+import { getUserProfile } from "../firebase/users-service";
 
 
 export const UserContext = createContext(null);
@@ -10,16 +11,22 @@ export function UserContextProvider({children}){
     const [user, setUser] = useState(null);
 
     useEffect (() => {
-        onAuthStateChanged(auth, (firebaseUser) => {
+        onAuthStateChanged(auth, async (firebaseUser) => {
             console.log(firebaseUser);
             if(firebaseUser){
-                //TODO: Update this
-                setUser({
-                    id: firebaseUser.uid,
-                    email: firebaseUser.email,
-                    name: firebaseUser.displayName,
+                
+                const profile = await getUserProfile(firebaseUser.email);
+                console.log('------------------ profile: ');
+                console.log(profile);
 
-                })
+                setUser(profile);
+                // //TODO: Update this
+                // setUser({
+                //     id: firebaseUser.uid,
+                //     email: firebaseUser.email,
+                //     name: firebaseUser.displayName,
+
+                // })
             }else{
                 setUser(null);
             }            
