7e93ffc7f0624022216d2d5f37c4c51257e477fe
Jennicoleperez <77292561+Jennicoleperez@users.noreply.github.com>
2023-02-27 20:12:32 -0400

    Add files via upload

---
+++ proyectoinicio/src/pages/Doctors/Doctors.jsx
@@ -0,0 +1,105 @@
+import React from 'react'
+import DoctorCard from './../../components/Cards/DoctorCard';
+import NavBar from '../../components/NavBar/NavBar';
+
+
+function Doctors() {
+  return (
+    <>
+        <NavBar/>
+        <div>    
+            <DoctorCard 
+                Nombre="Dr. Maria Carreño Pérez"
+                Género="Femenino"
+                Edad='40 años'
+                Especialización="Psicología infantil"
+                Experiencia='10 años'
+                Imagen="img/p1.jpeg"
+            />
+            
+            <DoctorCard 
+                Nombre="Dr. Maria Carreño Pérez"
+                Género="Femenino"
+                Edad='40 años'
+                Especialización="Psicología infantil"
+                Experiencia='10 años'
+                Imagen="img/p1.jpeg"
+            />
+            
+            <DoctorCard 
+                Nombre="Dr. Maria Carreño Pérez"
+                Género="Femenino"
+                Edad='40 años'
+                Especialización="Psicología infantil"
+                Experiencia='10 años'
+                Imagen="img/p1.jpeg"
+            />
+        
+            <DoctorCard 
+                Nombre="Dr. Maria Carreño Pérez"
+                Género="Femenino"
+                Edad='40 años'
+                Especialización="Psicología infantil"
+                Experiencia='10 años'
+                Imagen="img/p1.jpeg"
+            />
+
+            <DoctorCard 
+                Nombre="Dr. Maria Carreño Pérez"
+                Género="Femenino"
+                Edad='40 años'
+                Especialización="Psicología infantil"
+                Experiencia='10 años'
+                Imagen="img/p1.jpeg"
+            />
+
+                <DoctorCard 
+                Nombre="Dr. Maria Carreño Pérez"
+                Género="Femenino"
+                Edad='40 años'
+                Especialización="Psicología infantil"
+                Experiencia='10 años'
+                Imagen="img/p1.jpeg"
+            />
+            
+            <DoctorCard 
+                Nombre="Dr. Maria Carreño Pérez"
+                Género="Femenino"
+                Edad='40 años'
+                Especialización="Psicología infantil"
+                Experiencia='10 años'
+                Imagen="img/p1.jpeg"
+            />
+            
+            <DoctorCard 
+                Nombre="Dr. Maria Carreño Pérez"
+                Género="Femenino"
+                Edad='40 años'
+                Especialización="Psicología infantil"
+                Experiencia='10 años'
+                Imagen="img/p1.jpeg"
+            />
+        
+            <DoctorCard 
+                Nombre="Dr. Maria Carreño Pérez"
+                Género="Femenino"
+                Edad='40 años'
+                Especialización="Psicología infantil"
+                Experiencia='10 años'
+                Imagen="img/p1.jpeg"
+            />
+
+            <DoctorCard 
+                Nombre="Dr. Maria Carreño Pérez"
+                Género="Femenino"
+                Edad='40 años'
+                Especialización="Psicología infantil"
+                Experiencia='10 años'
+                Imagen="img/p1.jpeg"
+            />
+        </div>
+    </>
+  )
+}
+
+export default Doctors
